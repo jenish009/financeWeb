5193b0ea9bef2102a2188fa76c74c97c480c0fe3
Jenish <kavathiyajenish009@gmail.com>
2023-10-07 22:50:23 +0530

    add favicon icon

--- frontend/src/pages/Blog/index.jsx
+++ frontend/src/pages/Blog/index.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import Chip from '../../components/common/Chip';
-import EmptyList from '../../components/common/EmptyList';
-import './styles.css';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { BeatLoader } from 'react-spinners'; // Import BeatLoader
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Chip from "../../components/common/Chip";
+import EmptyList from "../../components/common/EmptyList";
+import "./styles.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { BeatLoader } from "react-spinners";
 
 const Blog = () => {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
-  const [loading, setLoading] = useState(true); // Initialize loading as true
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/post/getPostById?id=${id}`)
       .then((response) => {
-        setBlog(response.data); // Update the state with the fetched data
-        setLoading(false); // Set loading to false when data is fetched
+        setBlog(response.data);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
-        setLoading(false); // Set loading to false on error
+        console.error("Error fetching data:", error);
+        setLoading(false);
       });
   }, [id]);
 
   return (
     <>
-      <Link className='blog-goBack' to='/'>
+      <Link className="blog-goBack" to="/">
         <span> &#8592;</span> <span>Go Back</span>
       </Link>
 
-      {loading ? ( // Display loading indicator while loading is true
-        <div className='loading-container'>
-          <BeatLoader color={'#0f52ba'} loading={loading} size={20} />
+      {loading ? (
+        <div className="loading-container">
+          <BeatLoader color={"#0f52ba"} loading={loading} size={20} />
         </div>
       ) : blog ? (
-        <div className='blog-wrap'>
+        <div className="blog-wrap">
           <header>
-            <p className='blog-date'>Published {blog.createdAt}</p>
+            <p className="blog-date">Published {blog.createdAt}</p>
             <h1>{blog.title}</h1>
-            <div className='blog-subCategory'>
+            <div className="blog-subCategory">
               {blog.subCategory.map((category, i) => (
                 <div key={i}>
                   <Chip label={category} />
@@ -48,8 +48,24 @@ const Blog = () => {
               ))}
             </div>
           </header>
-          <img src={blog.cover} alt='cover' />
-          <p className='blog-desc'>{blog.description}</p>
+          <img src={blog.cover} alt="cover" />
+          <p className="blog-desc">{blog.description}</p>
+
+          {/* Content section */}
+          <div className="blog-content">
+            <h3>Content:</h3>
+            <p>{blog.content}</p>
+
+            {/* Author information */}
+            <div className="author-info">
+              <img
+                src={blog.authorAvatar}
+                alt="Author Avatar"
+                className="author-logo"
+              />
+              <p className="author-name">{blog.authorName}</p>
+            </div>
+          </div>
         </div>
       ) : (
         <EmptyList />
